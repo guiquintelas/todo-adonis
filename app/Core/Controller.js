09165124a1945618703e61085f3d453d1a3ef417
Guilherme Frota <guiquintelas96@gmail.com>
2019-05-15 19:58:16 -0300

    empty controller success becomes 204 status

--- app/Core/Controller.js
+++ app/Core/Controller.js
@@ -2,8 +2,15 @@
 
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 
+/** @type {import('lodash')} */
+const _ = use('lodash')
+
 class Controller {
   success (response, data, meta) {
+    if (_.isNil(data) && _.isNil(meta)) {
+      return response.status(204).json()
+    }
+
     return response.json({ data, meta })
   }
 
